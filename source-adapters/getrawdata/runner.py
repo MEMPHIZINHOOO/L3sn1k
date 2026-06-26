72eb2ee74bd10cf0aafabe3fdfcbdf8b44c7200a
MEMPHIZINHOOO <187868237+MEMPHIZINHOOO@users.noreply.github.com>
2026-06-26 14:55:11 -0400

    added security-header searching + tested

--- source-adapters/getrawdata/runner.py
+++ source-adapters/getrawdata/runner.py
@@ -1,12 +1,13 @@
 
 from crtadapter import fetchcrt
+from securityheaders import getheaders
 
 def get_domain():  #gets the user input (domain)
     return input("Enter your target domain: ")
 
 domain=get_domain()
 result =fetchcrt(domain) #runs the adapter for this domain
-
+result2 = getheaders(domain)
 print(result)
 
 
