0a60f16476be842151320a123521174d080deaba
m3mph1s <187868237+MEMPHIZINHOOO@users.noreply.github.com>
2026-06-27 17:27:23 +0100

    added cors checker...tested (need more test cases and response analysis)

--- source-adapters/getrawdata/runner.py
+++ source-adapters/getrawdata/runner.py
@@ -1,14 +1,16 @@
 
 from crtadapter import fetchcrt
 from securityheaders import getheaders
+from corschecker import corscheck
 
 def get_domain():  #gets the user input (domain)
     return input("Enter your target domain: ")
 
 domain=get_domain()
-result =fetchcrt(domain) #runs the adapter for this domain
-result2 = getheaders(domain)
-print(result)
+#result =fetchcrt(domain) #runs the adapter for this domain
+#result2 = getheaders(domain)
+result3= corscheck(domain)
+print(result3)
 
 
 
