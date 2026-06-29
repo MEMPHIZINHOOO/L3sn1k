31c279bc323cf3e959ef1529824d70938ef185a0
Cosm <leopguerreiro@gmail.com>
2026-06-29 19:48:11 +0100

    got new proxy almost up and running

--- l3snik/src/main.rs
+++ l3snik/src/main.rs
@@ -1,14 +1,24 @@
 // use std::sync::mpsc;
 use std::env;
 use std::thread;
-use tools::proxy::proxy_run;
+use std::net::SocketAddr;
+
+use tools::proxy::instantiate_proxy;
+
 mod gui;
 mod tests;
-fn main() -> iced::Result {
-    let mut arguments: Vec<String> = env::args().collect();
-    let thread_number = arguments.pop();
 
-    thread::spawn(||  {proxy_run(thread_number.unwrap_or("4".to_string()).parse::<usize>().expect("no thread number specified"))});
+use tokio::sync::mpsc;
+use proxelar::ProxyEvent;
+
+fn main() -> iced::Result {
+    
+    let mut arguments: Vec<String> = env::args().collect();
+    let port_input = arguments.pop();
+    let address: SocketAddr = SocketAddr::new(SocketAddrV4::new(127, 0, 0, 1), port_input.parse().expect("not a valid port"));
+
+    let (tx_sender, tx_receiver) = mpsc::channel(1); 
+    thread::spawn(||  {instantiate_proxy(address, tx_sender)});
 
     unsafe {
         std::env::set_var("WGPU_POWER_PREF", "high");
