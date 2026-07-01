16efe82ac8117c56f5d34db92a9d1bd5286ddb3a
Cosm <leopguerreiro@gmail.com>
2026-07-01 23:17:14 +0100

    bazinga

--- l3snik/src/main.rs
+++ l3snik/src/main.rs
@@ -2,7 +2,7 @@
 use std::env;
 use std::net::{SocketAddr,IpAddr, Ipv4Addr};
 use std::error::Error;
-use tools::proxy::instantiate_proxy;
+use tools::proxy::{check_init_ca_dir,instantiate_proxy};
 
 mod gui;
 mod tests;
@@ -12,18 +12,19 @@ use tokio::sync::mpsc;
 
 #[main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    
+
+        
     let mut arguments: Vec<String> = env::args().collect();
     let port_input = arguments.pop();
     let address: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port_input.expect("not a valid port").parse()?);
 
     // these channels will be useful, soon enough
-    let (tx_sender, _tx_receiver) = mpsc::channel(1); 
+    let (tx_sender, tx_receiver) = mpsc::channel(1); 
     tokio::spawn(instantiate_proxy(address, tx_sender));
 
     unsafe {
         std::env::set_var("WGPU_POWER_PREF", "high");
     }
 
-    gui::L3snikGui::start()
+    gui::L3snikGui::start(tx_receiver)
 } 
--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 
-
 export OPENSSL_NO_VENDOR=1
 cargo run -- 8080
