9101c536ee6d1c22cb16e5d27b2a446b1a448e4d
Cosm <leopguerreiro@gmail.com>
2026-07-01 12:47:24 +0100

    got self signing up and running and async functionality working, proxy should work now

--- l3snik/src/main.rs
+++ l3snik/src/main.rs
@@ -1,6 +1,5 @@
 // use std::sync::mpsc;
 use std::env;
-use std::thread;
 use std::net::{SocketAddr,IpAddr, Ipv4Addr};
 use std::error::Error;
 use tools::proxy::instantiate_proxy;
@@ -8,16 +7,19 @@ use tools::proxy::instantiate_proxy;
 mod gui;
 mod tests;
 
+use tokio::main;
 use tokio::sync::mpsc;
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[main]
+async fn main() -> Result<(), Box<dyn Error>> {
     
     let mut arguments: Vec<String> = env::args().collect();
     let port_input = arguments.pop();
     let address: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port_input.expect("not a valid port").parse()?);
 
+    // these channels will be useful, soon enough
     let (tx_sender, _tx_receiver) = mpsc::channel(1); 
-    thread::spawn( move ||  {instantiate_proxy(address, tx_sender)});
+    tokio::spawn(instantiate_proxy(address, tx_sender));
 
     unsafe {
         std::env::set_var("WGPU_POWER_PREF", "high");
